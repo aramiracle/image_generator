6f1fe303b4207d69bf990a9750d13286f13946a8
aramiracle <alireza@arch-x1.localdomain>
2023-09-21 11:01:46 +0330

    encapsulating

--- model.py
+++ model.py
@@ -1,40 +1,5 @@
 import torch
 import torch.nn as nn
-
-
-# Define the Generator network
-class PretrainGenerator(nn.Module):
-    def __init__(self):
-        super(PretrainGenerator, self).__init__()
-        
-        # Define additional layers for upsampling to reach 50x50 resolution
-        self.fc = nn.Sequential(
-            nn.Linear(7000, 1000),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(1000, 128)
-        )
-        
-        self.deconv1 = self.deconv_block(128, 64)
-        self.deconv2 = self.deconv_block(64, 32)
-        self.deconv3 = self.deconv_block(32, 16)
-        self.deconv4 = self.deconv_block(16, 8)
-        self.deconv5 = self.deconv_block(8, 3)
-
-    def deconv_block(self, in_channel, out_channel):
-        return nn.Sequential(
-            nn.ConvTranspose2d(in_channel, out_channel, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2, inplace=True)
-        )
-    
-    def forward(self, x):
-        x0 = self.fc(x).unsqueeze(2).unsqueeze(3) #128x1x1
-        x1 = self.deconv1(x0) #64x2x2
-        x2 = self.deconv2(x1) #32x4x4
-        x3 = self.deconv3(x2) #16x8x8
-        x4 = self.deconv4(x3) #8x16x16
-        output = self.deconv5(x4) #3x32x32
-
-        return output
     
 
 class PCAGenerator(nn.Module):
--- train_pca.py
+++ train_pca.py
@@ -2,7 +2,6 @@ import os
 import torch
 from torchvision.transforms import transforms
 from sklearn.decomposition import PCA
-import torchvision.models as models
 from PIL import Image
 from torchvision.utils import save_image
 from model import *
@@ -16,185 +15,180 @@ from utils import *
 from data_loader import ImageDataset
 import torchvision.models as models
 
-train_root_dir = 'data/resized'
+def calculate_features(train_root_dir, features_dir):
+    transform = transforms.Compose([transforms.ToTensor()])
+    image_names = sorted(os.listdir(train_root_dir))
 
-transform = transforms.Compose([transforms.ToTensor()])
+    if os.listdir(features_dir):
+        features_file_name = os.listdir(features_dir)
+        X_pca_normalized = torch.load(os.path.join(features_dir, features_file_name[0]))
+        print('Features loaded.')
+    else:
+        # Calculate images feature with pretrained models
+        model_1 = models.efficientnet_b2(weights='EfficientNet_B2_Weights.DEFAULT').eval()
+        model_2 = models.shufflenet_v2_x2_0(weights='ShuffleNet_V2_X2_0_Weights.DEFAULT').eval()
+        model_3 = models.regnet_y_1_6gf(weights='RegNet_Y_1_6GF_Weights.DEFAULT').eval()
+        model_4 = models.densenet121(weights='DenseNet121_Weights.DEFAULT').eval()
+        model_5 = models.mnasnet1_3(weights='MNASNet1_3_Weights.DEFAULT').eval()
+        model_6 = models.mobilenet_v3_large(weights='MobileNet_V3_Large_Weights.DEFAULT').eval()
+        model_7 = models.regnet_x_1_6gf(weights='RegNet_X_1_6GF_Weights.DEFAULT').eval()
 
-image_names = sorted(os.listdir(train_root_dir))
+        features_list = []
+        for image_name in tqdm(image_names):
+            image = Image.open(f'{train_root_dir}/{image_name}').convert('RGB')
+            image_tensor = transform(image).unsqueeze(0)
 
-print('Starting calculating features and transform it with PCA if is it not saved.')
+            f1 = model_1(image_tensor)
+            f2 = model_2(image_tensor)
+            f3 = model_3(image_tensor)
+            f4 = model_4(image_tensor)
+            f5 = model_5(image_tensor)
+            f6 = model_6(image_tensor)
+            f7 = model_7(image_tensor)
 
-features_dir = 'features'
-os.makedirs(features_dir, exist_ok=True)
+            feature = torch.cat((f1, f2, f3, f4, f5, f6, f7), dim=1)
+            features_list.append(feature)
 
-if os.listdir(features_dir):
-    features_file_name = os.listdir(features_dir)
-    X_pca_normalized = torch.load('features/transformed_features.pt')
+        print('Features calculated.')
 
-    print('Features loaded.')
-else:
-    # Calculate images feature with pretrained models
-    model_1 = models.efficientnet_b2(weights='EfficientNet_B2_Weights.DEFAULT').eval()
-    model_2 = models.shufflenet_v2_x2_0(weights='ShuffleNet_V2_X2_0_Weights.DEFAULT').eval()
-    model_3 = models.regnet_y_1_6gf(weights='RegNet_Y_1_6GF_Weights.DEFAULT').eval()
-    model_4 = models.densenet121(weights='DenseNet121_Weights.DEFAULT').eval()
-    model_5 = models.mnasnet1_3(weights='MNASNet1_3_Weights.DEFAULT').eval()
-    model_6 = models.mobilenet_v3_large(weights='MobileNet_V3_Large_Weights.DEFAULT').eval()
-    model_7 = models.regnet_x_1_6gf(weights='RegNet_X_1_6GF_Weights.DEFAULT').eval()
+        features_arr = torch.cat(features_list, dim=0).detach().numpy()
+        pca = PCA(n_components=256)
+        X_pca = torch.tensor(pca.fit_transform(features_arr))
+        X_pca_normalized = normalize(X_pca)
+        print(f'Explained variance: {pca.explained_variance_ratio_.sum()}')
+        torch.save(X_pca_normalized, f'{features_dir}/transformed_features.pt')
+        print('Transformed features are saved')
 
-    features_list = []
-    for image_name in tqdm(image_names):
-        image = Image.open(f'{train_root_dir}/{image_name}').convert('RGB')
-        image_tensor = transform(image).unsqueeze(0)
+    return X_pca_normalized
 
-        f1 = model_1(image_tensor)
-        f2 = model_2(image_tensor)
-        f3 = model_3(image_tensor)
-        f4 = model_4(image_tensor)
-        f5 = model_5(image_tensor)
-        f6 = model_6(image_tensor)
-        f7 = model_7(image_tensor)
-
-        feature = torch.cat((f1, f2, f3, f4, f5, f6, f7), dim=1)
-        features_list.append(feature)
-
-    print('Features calculated.')
-
-    features_arr = torch.cat(features_list, dim=0).detach().numpy()
-    pca = PCA(n_components=256)
-    X_pca = torch.tensor(pca.fit_transform(features_arr))
-    X_pca_normalized = normalize(X_pca)
-    print(f'Explained variance: {pca.explained_variance_ratio_.sum()}')
-    torch.save(X_pca_normalized, f'{features_dir}/transformed_features.pt')
+def train_pca_generator(train_root_dir, batch_size, learning_rate, epochs, transformed_features):
+    generator = PCAGenerator()
+    optimizer_G = optim.Adam(generator.parameters(), lr=learning_rate)
     
-    print('Transformed features is saved')
-
-#Training
-
-# Define hyperparameters
-batch_size = 64
-learning_rate = 1e-3
-epochs = 5000
-
-generator = PCAGenerator()
-criterion = nn.BCELoss()
-optimizer_G = optim.Adam(generator.parameters(), lr=learning_rate)
-
-transform = transforms.Compose([transforms.ToTensor()])
-train_dataset = ImageDataset(train_root_dir, transform=transform, train=True)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
-
-
-generated_images_dir = 'generated_images'
-shutil.rmtree(generated_images_dir, ignore_errors=True)
-os.makedirs(generated_images_dir, exist_ok=True)
-
-# Define the path to the checkpoint directory
-checkpoint_dir = 'saved_models/pca/'
-os.makedirs(checkpoint_dir, exist_ok=True)
-
-ssim_metric = StructuralSimilarityIndexMeasure(data_range=1)
-psnr_metric = PeakSignalNoiseRatio()
-
-
-
-# List all checkpoint files in the directory
-checkpoint_files = [f for f in os.listdir(checkpoint_dir) if re.match(r'gan_checkpoint_epoch\d+\.pth', f)]
-
-if checkpoint_files:
-    # Extract and sort the epoch numbers
-    epoch_numbers = [int(re.search(r'gan_checkpoint_epoch(\d+)\.pth', f).group(1)) for f in checkpoint_files]
-    epoch_numbers.sort()
+    transform = transforms.Compose([transforms.ToTensor()])
+    train_dataset = ImageDataset(train_root_dir, transform=transform, train=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
     
-    # Load the latest checkpoint
-    latest_epoch = epoch_numbers[-1]
-    latest_checkpoint = f'gan_checkpoint_epoch{latest_epoch:04d}.pth'
-    checkpoint_path = os.path.join(checkpoint_dir, latest_checkpoint)
+    generated_images_dir = 'generated_images'
+    shutil.rmtree(generated_images_dir, ignore_errors=True)
+    os.makedirs(generated_images_dir, exist_ok=True)
     
-    # Load the generator and discriminator models and their states
-    checkpoint = torch.load(checkpoint_path)
-    epoch = checkpoint['epoch']
-    generator.load_state_dict(checkpoint['generator_state_dict'])
-    optimizer_G.load_state_dict(checkpoint['optimizer_G_state_dict'])
-
-    # Make sure to set the mode for generator and discriminator
-    generator.train()
-
-    print(f"PCA - Loaded checkpoint from epoch {epoch}. Resuming training...")
-else:
-    epoch = 0
-    print("PCA - No checkpoint found. Starting training from epoch 1...")
-
-
-best_ssim = 0
-
-# Training loop phase I
-for epoch in range(epoch, epochs):
-    running_psnr = 0.0
-    running_ssim = 0.0
-    running_lnl1 = 0.0
-    running_loss = 0.0
-
-    # Wrap train_loader with tqdm for progress bar
-    for batch_idx, real_images in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}")):
-
-        current_batch_size = real_images.shape[0]
+    # Define the path to the checkpoint directory
+    checkpoint_dir = 'saved_models/pca/'
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    
+    ssim_metric = StructuralSimilarityIndexMeasure(data_range=1)
+    psnr_metric = PeakSignalNoiseRatio()
+    
+    # List all checkpoint files in the directory
+    checkpoint_files = [f for f in os.listdir(checkpoint_dir) if re.match(r'gan_checkpoint_epoch\d+\.pth', f)]
+    
+    if checkpoint_files:
+        # Extract and sort the epoch numbers
+        epoch_numbers = [int(re.search(r'gan_checkpoint_epoch(\d+)\.pth', f).group(1)) for f in checkpoint_files]
+        epoch_numbers.sort()
         
-        features = X_pca_normalized[batch_idx*batch_size:batch_idx*batch_size + current_batch_size]
-
-        # Generate fake images from the generator
-        fake_images = generator(features)
-        if not((batch_idx + 1)%4):
-            save_image(torch.cat((fake_images, real_images), dim=0), f'{generated_images_dir}/fake_image_epoch{epoch + 1:04d}_batch{batch_idx + 1:02d}.png')
-
-        # Calculate PSNR for this batch and accumulate
-        psnr = psnr_metric(fake_images, real_images)
-        running_psnr += psnr
+        # Load the latest checkpoint
+        latest_epoch = epoch_numbers[-1]
+        latest_checkpoint = f'gan_checkpoint_epoch{latest_epoch:04d}.pth'
+        checkpoint_path = os.path.join(checkpoint_dir, latest_checkpoint)
+        
+        # Load the generator and discriminator models and their states
+        checkpoint = torch.load(checkpoint_path)
+        epoch = checkpoint['epoch']
+        generator.load_state_dict(checkpoint['generator_state_dict'])
+        optimizer_G.load_state_dict(checkpoint['optimizer_G_state_dict'])
     
-        # Calculate SSIM for this batch and accumulate
-        ssim_value = ssim_metric(fake_images, real_images)
-        running_ssim += ssim_value
-
-        # Calculate LNL1 for this batch and accumulate
-        lnl1_value = lnl1_metric(fake_images, real_images)
-        running_lnl1 += lnl1_value
-
-        
-        # Train Generator
-        optimizer_G.zero_grad()
-        
-        # Calculating custom loss
-        loss = PSNR_SSIM_LNL1_loss(fake_images, real_images)
-        running_loss += loss
-
-        loss.backward()
-        optimizer_G.step()
-        
+        # Make sure to set the mode for generator and discriminator
+        generator.train()
     
-    average_psnr = running_psnr / len(train_loader)
-    average_ssim = running_ssim / len(train_loader)
-    average_lnl1 = running_lnl1 / len(train_loader)
-    average_loss = running_loss / len(train_loader)
+        print(f"PCA - Loaded checkpoint from epoch {epoch}. Resuming training...")
+    else:
+        epoch = 0
+        print("PCA - No checkpoint found. Starting training from epoch 1...")
+    
+    best_ssim = 0
+    
+    # Training loop phase I
+    for epoch in range(epoch, epochs):
+        running_psnr = 0.0
+        running_ssim = 0.0
+        running_lnl1 = 0.0
+        running_loss = 0.0
+    
+        # Wrap train_loader with tqdm for progress bar
+        for batch_idx, real_images in enumerate(tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}")):
+    
+            current_batch_size = real_images.shape[0]
+            
+            features = transformed_features[batch_idx*batch_size:batch_idx*batch_size + current_batch_size]
+    
+            # Generate fake images from the generator
+            fake_images = generator(features)
+            if not((batch_idx + 1)%4):
+                save_image(torch.cat((fake_images, real_images), dim=0), f'{generated_images_dir}/fake_image_epoch{epoch + 1:04d}_batch{batch_idx + 1:02d}.png')
+    
+            # Calculate PSNR for this batch and accumulate
+            psnr = psnr_metric(fake_images, real_images)
+            running_psnr += psnr
+        
+            # Calculate SSIM for this batch and accumulate
+            ssim_value = ssim_metric(fake_images, real_images)
+            running_ssim += ssim_value
+    
+            # Calculate LNL1 for this batch and accumulate
+            lnl1_value = lnl1_metric(fake_images, real_images)
+            running_lnl1 += lnl1_value
+    
+            
+            # Train Generator
+            optimizer_G.zero_grad()
+            
+            # Calculating custom loss
+            loss = PSNR_SSIM_LNL1_loss(fake_images, real_images)
+            running_loss += loss
+    
+            loss.backward()
+            optimizer_G.step()
+        
+        average_psnr = running_psnr / len(train_loader)
+        average_ssim = running_ssim / len(train_loader)
+        average_lnl1 = running_lnl1 / len(train_loader)
+        average_loss = running_loss / len(train_loader)
+    
+        print(f"PCA - Epoch [{epoch + 1}/{epochs}] Loss G: {average_loss:.4f}")
+        print(f'PCA - Epoch [{epoch + 1}/{epochs}] Metrics: PSNR: {average_psnr:.4f} SSIM: {average_ssim:.4f} LNL1: {average_lnl1:.4f}')
+    
+        # Check the condition and save models if met
+        if not((epoch + 1) % 5):
+            save_path = f'{checkpoint_dir}/gan_checkpoint_epoch{epoch + 1:04d}.pth'
+            torch.save({
+                'epoch': epoch + 1,
+                'generator_state_dict': generator.state_dict(),
+                'optimizer_G_state_dict': optimizer_G.state_dict()
+            }, save_path)
+            print('PCA - Model is saved.')
+    
+        if best_ssim < average_ssim:
+            best_ssim = average_ssim
+            save_path = f'{checkpoint_dir}/best_gan_checkpoint.pth'
+            torch.save({
+                'generator_state_dict': generator.state_dict()
+            }, save_path)
+            print('$$$ Best model is saved according to SSIM. $$$')
 
-    print(f"PCA - Epoch [{epoch + 1}/{epochs}] Loss G: {average_loss:.4f}")
-    print(f'PCA - Epoch [{epoch + 1}/{epochs}] Metrics: PSNR: {average_psnr:.4f} SSIM: {average_ssim:.4f} LNL1: {average_lnl1:.4f}')
+if __name__ == "__main__":
+    train_root_dir = 'data/resized'
+    features_dir = 'features'
 
-    # Check the condition and save models if met
-    if not((epoch + 1) % 5):
-        save_path = f'{checkpoint_dir}/gan_checkpoint_epoch{epoch + 1:04d}.pth'
-        torch.save({
-            'epoch': epoch + 1,
-            'generator_state_dict': generator.state_dict(),
-            'optimizer_G_state_dict': optimizer_G.state_dict()
-        }, save_path)
-        print('PCA - Model is saved.')
+    # Calculate features and apply PCA if not saved
+    print('Starting calculating features and transform it with PCA if it is not saved.')
+    os.makedirs(features_dir, exist_ok=True)
+    X_pca_normalized = calculate_features(train_root_dir, features_dir)
 
-    if best_ssim < average_ssim:
-        best_ssim = average_ssim
-        save_path = f'{checkpoint_dir}/best_gan_checkpoint.pth'
-        torch.save({
-            'generator_state_dict': generator.state_dict()
-        }, save_path)
-        print('$$$ Best model is saved according to SSIM. $$$')
+    # Training
+    batch_size = 64
+    learning_rate = 1e-3
+    epochs = 5004
 
-
+    train_pca_generator(train_root_dir, batch_size, learning_rate, epochs, X_pca_normalized)
